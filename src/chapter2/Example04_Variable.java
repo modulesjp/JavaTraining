9b69a23d00f99b792de43b4eaca3d1498b5c0ee1
node <node@NODE4M981C>
2018-02-06 03:47:36 +0900

    Update 2018.02.06

--- src/chapter2/Example04_Variable.java
+++ src/chapter2/Example04_Variable.java
@@ -6,17 +6,31 @@ package chapter2;
  */
 public class Example04_Variable {
 	public static void main(String[] args) {
-		// データ型△変数名;
-		int i = 1212;
+		int a; // 変数の宣言　「データ型△変数名;」
+		a = 1203; // 値の代入　「変数△=△値;」
+		
+		/* 宣言と代入を1行で表現 */
+		int b = 100; 
+		
+		/* 変数の利用 */
+		System.out.println(a);
+		System.out.println(b);
+		
+		/* 値の代入 */
+		b = 200;
+		System.out.println(b);
+		
+		/* その他のデータ型 */
 		char c = 'a';
-		boolean b = true;
+		boolean bo = true;
 		
-		// ※文字列は基本データ型ではない
-		String str = "Hello";
+		/* Pick up
+		 * 「String」は基本データ型ではない
+		 */
+		String str = "Hello"; // 参照型
 		
-		System.out.println(i);
-		
-		// JavaではUnicodeが使われている
-		char u = '\u4032';
+		/* JavaではUnicodeが使われている */
+		char u = '\u3042';
+		System.out.println(u);
 	}
 }
