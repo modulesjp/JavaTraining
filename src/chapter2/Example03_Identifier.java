56b3b040b8c5a9d066df62e9ae50033830d2ca5d
node <node@NODE4M981C>
2018-02-06 02:53:09 +0900

    Add 2018.02.06

---
+++ src/chapter2/Example03_Identifier.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+/**
+ * 識別子
+ *
+ */
+public class Example03_Identifier {
+	public static void main(String[] args) {
+		// ▼識別子の規則
+		// １．Javaであらかじめ定義されている文字は使用不可
+		// ２．使用できる文字は数字（0~9）、アルファベット（A~Z、a~z）、アンダースコア（_）、ドルマーク（$）
+		// ３．先頭に数字は使用不可
+	}
+	
+	/* 「public」は識別子なので、以下メソッドはコンパイルエラーが発生する
+	public static public() {
+		System.out.println("Hello");
+	}
+	*/
+}
---
+++ src/chapter2/Example04_Variable.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+/**
+ * 変数
+ * 基本データ型（プリミティブ型）
+ */
+public class Example04_Variable {
+	public static void main(String[] args) {
+		// データ型△変数名;
+		int i = 1212;
+		char c = 'a';
+		boolean b = true;
+		
+		// ※文字列は基本データ型ではない
+		String str = "Hello";
+		
+		System.out.println(i);
+		
+		// JavaではUnicodeが使われている
+		char u = '\u4032';
+	}
+}
