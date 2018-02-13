ffe163b885947c4246cbec38ad043e53c38f7845
node <node@NODE4M981C>
2018-02-14 00:16:18 +0900

    Add 2018.02.13

---
+++ src/chapter2/Example15_while.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+/**
+ * while文
+ *
+ */
+public class Example15_while {
+	public static void main(String[] args) {
+		int cnt = 1;
+		int sum = 0;
+		while(cnt <= 10) {
+			sum += cnt;
+			cnt++;
+		}
+		System.out.println("1から10までの総和は" + sum + "です。");
+	}
+}
