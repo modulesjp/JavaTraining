4a4b96140b411f49c32ac49296f966aab64a1998
node <node@NODE4M981C>
2018-02-17 00:39:33 +0900

    Add 2018.02.17

---
+++ projects/projects/fundamentals/chapter2/Ex018_Continue.java
@@ -0,0 +1,24 @@
+package projects.fundamentals.chapter2;
+
+/**
+ * continue文
+ *
+ */
+public class Ex018_Continue {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			if (i == 5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
+
+/*
+ *############################
+ *# Note : 
+ *# iが5の場合、
+ *# 以降の処理はスキップされるが、ループは繰り返される
+ *#############################
+ */
