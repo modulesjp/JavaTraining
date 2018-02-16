57939eb56ed60c7aed23bc1a95e7d291a148cf4b
node <node4m@gmail.com>
2018-02-16 12:57:06 +0900

    Add 2018.02.16

---
+++ src/practice/fundamentals/chapter2/Practice02.java
@@ -0,0 +1,11 @@
+package practice.fundamentals.chapter2;
+
+/*
+ * 次のプログラムを「Hello Java」が
+ * 表示されるように修正してください。
+ */
+public class Practice02 {
+	public void static main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
