5e6f1b8e9bac55342efe39c4ffb98070547ba599
node <node4m@gmail.com>
2018-02-17 01:08:54 +0900

    Add 2018.02.17

---
+++ practice/fundamentals/chapter2/No003.java
@@ -0,0 +1,14 @@
+package practice.fundamentals.chapter2;
+
+/*
+ * 実習03 - 変数
+ */
+public class No003 {
+	/*
+	 * 変数を使って「x = 23」を表示させてください
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+}
