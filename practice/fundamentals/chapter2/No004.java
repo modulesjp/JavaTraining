3b6975609c29adb59f94c8ce92185be892ad3b43
node <node4m@gmail.com>
2018-02-19 12:17:30 +0900

    Add 2018.02.19

---
+++ practice/fundamentals/chapter2/No004.java
@@ -0,0 +1,13 @@
+package fundamentals.chapter2;
+
+/*
+ * 実習004 - 算術演算子
+ */
+public class No004 {
+    /*
+     * 2つの整数を定義して、その和を求めて表示させてください。
+     */
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ practice/fundamentals/chapter2/No005.java
@@ -0,0 +1,13 @@
+package.fundamentals.chapter2;
+
+/*
+ * 算術演算子
+ */
+public class No005 {
+    /*
+     * 三角形の面積を求めて結果を表示させてください(サイズは任意)
+     */
+     public static void main(String[] args) {
+     
+     }
+}
