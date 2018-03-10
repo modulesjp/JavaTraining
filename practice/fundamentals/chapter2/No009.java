d72b8f62a58a90ac1db67bb07a13bd7671afabf3
node <node@NODE4M981C>
2018-03-11 01:37:20 +0900

    Change Name

---
+++ practice/fundamentals/chapter2/No009.java
@@ -0,0 +1,11 @@
+package fundamentals.chapter2;
+
+/*
+ * 2つの整数を定義して、その和を求めて表示させてください。
+ */
+public class No009 {
+    
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ practice/fundamentals/chapter2/No010.java
@@ -0,0 +1,11 @@
+package fundamentals.chapter2;
+
+/*
+ * 三角形の面積を求めて結果を表示させてください(サイズは任意)
+ */
+public class No010 {
+    
+     public static void main(String[] args) {
+     
+     }
+}
