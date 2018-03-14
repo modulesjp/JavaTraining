9fc1e7f9689c58c9bca922986f9078819854f450
node <node@NODE4M981C>
2018-03-14 22:33:01 +0900

    Add 2018.03.14

--- practice/fundamentals/chapter2/No007.java
+++ practice/fundamentals/chapter2/No007.java
@@ -1,5 +1,16 @@
 package fundamentals.chapter2;
 
+/*
+ * 次のプログラムの間違っている箇所を修正して
+ * 正常に実行できるようにしてください。
+ */
 public class No007 {
-
+	public static viod main(String arsg[]) {
+		byte b = 0;
+		int i = 2;
+		b = i;
+		
+		System.out.println("b=" + b);
+		System.out.println("i=" + i);
+	}
 }
--- practice/fundamentals/chapter2/No008.java
+++ practice/fundamentals/chapter2/No008.java
@@ -1,5 +1,16 @@
 package fundamentals.chapter2;
 
+/*
+ * 次のプログラムの間違っている箇所を修正して
+ * 正常に実行できるようにしてください。
+ */
 public class No008 {
-
+	public static void main(String[] args) {
+		float f = 1.2;
+		double d = 5.275;
+		f = d;
+		
+		System.out.println("f=" + f);
+		System.out.println("d=" + d);
+	}
 }
