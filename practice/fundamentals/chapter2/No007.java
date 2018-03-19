7cba3cdf85ab3259ef017442faf65bdca39cf1b2
Node4m <node4m@gmail.com>
2018-03-19 22:51:37 +0900

    Add 2018.03.19

---
+++ practice/fundamentals/chapter2/No007.java
@@ -0,0 +1,11 @@
+package fundamentals.chapter2;
+
+/*
+ * 次のプログラムをEclipseではなくコマンドプロンプトから実行し、
+ * どのようなエラーメッセージが表示されるか確認してください。
+ */
+public class No007 {
+	public static void main(String[] args) {
+		System.out.println("Compile Error")
+	}
+}
