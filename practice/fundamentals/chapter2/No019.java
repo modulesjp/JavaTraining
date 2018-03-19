c1e29c2b63a1d862e741e3d0032a1d61d3ef569e
Node4m <node4m@gmail.com>
2018-03-19 23:07:42 +0900

    Add 2018.03.19

---
+++ practice/fundamentals/chapter2/No019.java
@@ -0,0 +1,9 @@
+package fundamentals.chapter2;
+
+/*
+ * 半径4mの円の面積を求めて表示するプログラムを作成してください。
+ * 円周率は「3.14」とすること。
+ */
+public class No019 {
+
+}
---
+++ practice/fundamentals/chapter2/No023.java
@@ -0,0 +1,8 @@
+package fundamentals.chapter2;
+
+/*
+ * 要素型がdoubleで要素数が5の配列を生成して、その全要素を表示するプログラムを作成してください。
+ */
+public class No023 {
+
+}
