a127b8aa45200adbbb2cbcf52c251808e0074c51
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-03 15:16:20 +0900

    2018.04.03

--- projects/fundamentals/chapter2/Ex016_Nest.java
+++ projects/fundamentals/chapter2/Ex016_Nest.java
@@ -9,11 +9,7 @@ public class Ex016_Nest {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				int k = (i + 1)*(j + 1);
-				if (k < 10) {
-					System.out.print(" " + k);
-				} else {
-					System.out.print(" " + k);
-				}
+				System.out.print(" " + k);
 			}
 			System.out.println();
 		}
