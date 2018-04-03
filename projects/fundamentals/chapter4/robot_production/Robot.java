0d06e82ad15edd45f27e73220a1cd81dd8a69833
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-03 18:02:32 +0900

    2018.04.03

---
+++ projects/fundamentals/chapter4/robot_production/Robot.java
@@ -0,0 +1,11 @@
+package fundamentals.chapter4.robot_production;
+
+/*
+ * ロボットクラス
+ */
+public class Robot {
+	// 話す機能の実装
+	public void talk() {
+		System.out.println("私の名前はロボットです。");
+	}
+}
---
+++ projects/fundamentals/chapter4/robot_production/RobotProduction.java
@@ -0,0 +1,11 @@
+package fundamentals.chapter4.robot_production;
+
+/*
+ * ロボット生成クラス
+ */
+public class RobotProduction {
+	public static void main(String[] args) {
+		Robot robot = new Robot(); // ロボット生成
+		robot.talk();
+	}
+}
