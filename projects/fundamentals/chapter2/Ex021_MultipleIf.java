038f01a80c405021a17798542507976d8b080bd4
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-03 16:21:04 +0900

    2018.04.03

--- projects/fundamentals/chapter2/Ex021_MultipleIf.java
+++ projects/fundamentals/chapter2/Ex021_MultipleIf.java
@@ -8,12 +8,12 @@ public class Ex021_MultipleIf {
 	public static void main(String[] args) {
 		int score = 70;
 		
-		if (score < 30) {
-			System.out.println("不合格");
-		} else if (score < 70) {
+		if (score >= 80) {
+			System.out.println("合格");
+		} else if (score >= 70) {
 			System.out.println("追試");
 		} else {
-			System.out.println("合格");
+			System.out.println("不合格");
 		}
 	}
 }
