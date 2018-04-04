cfa5104d6575fd7968d3499f96a8f759b4d561d9
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-04 19:14:09 +0900

    2018.04.04

---
+++ projects/fundamentals/chapter4/Ex316_anonymous/Sample3_23.java
@@ -0,0 +1,13 @@
+package fundamentals.chapter4.Ex316_anonymous;
+
+public class Sample3_23 {
+
+}
+
+class AnonytySuper {
+	private int i = 0;
+	public void display() {
+		System.out.println("This is MySuper Class.");
+		System.out.println("i = " + i);
+	}
+}
