ab53ff073565dfc8a45483cace4ac57de0b9c2d1
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-04 19:14:24 +0900

    2018.04.04

--- projects/fundamentals/chapter4/Ex316_anonymous/Sample3_23.java
+++ projects/fundamentals/chapter4/Ex316_anonymous/Sample3_23.java
@@ -1,13 +1,21 @@
 package fundamentals.chapter4.Ex316_anonymous;
 
 public class Sample3_23 {
-
+	public static void main(String[] args) {
+		
+	}
 }
 
-class AnonytySuper {
+class AnonymitySuper {
 	private int i = 0;
 	public void display() {
 		System.out.println("This is MySuper Class.");
 		System.out.println("i = " + i);
 	}
+}
+
+class AnonymityOuther {
+	private int size = 5;
+	
+	
 }
