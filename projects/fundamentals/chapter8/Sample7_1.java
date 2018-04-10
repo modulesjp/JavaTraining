aea2c4cff85da5f0917ca565783f904468d02af8
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-10 19:38:33 +0900

    2018.04.10

--- projects/fundamentals/chapter8/Sample7_1.java
+++ projects/fundamentals/chapter8/Sample7_1.java
@@ -1,13 +1,23 @@
 package fundamentals.chapter8;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Sample7_1 {
-	public static void main(String[] args) {
-		File dir = new File(".");
-		String listfile[] = dir.list();
-		for (int i= 0; i < listfile.length; i++) {
-			System.out.println(listfile[i]);
+	public static void main(String[] args) throws IOException {
+		
+		// Fileオブジェクトの生成
+		File dir = new File("c:\\training\\Hello.java");
+		
+		// Fileクラスのメソッド使用例
+		// createNewFile()  ・・・  ファイルが存在しなければ新規作成
+		if (dir.createNewFile()) {
+			System.out.println(dir.getName() + "を新規作成しました。");
+		} else {
+			System.out.println("既に存在します。");
 		}
+		
+		// getAbsolutePath()  ・・・  ファイルまでのパスを返す
+		System.out.println(dir.getAbsolutePath());
 	}
 }
