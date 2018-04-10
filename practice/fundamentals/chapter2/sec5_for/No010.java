978f660e407e51d66479bb04b320a45c78e392a3
Node4m <node4m@gmail.com>
2018-04-11 00:57:32 +0900

    2018.04.10

--- practice/fundamentals/chapter2/sec5_for/No010.java
+++ practice/fundamentals/chapter2/sec5_for/No010.java
@@ -1,6 +1,6 @@
 package fundamentals.chapter2.sec5_for;
 /*
- * String型の配列に値1~5を代入し、拡張for文を使用して1から順番に表示させるプログラムを作成してください。
+ * String型の配列にを宣言し、拡張for文を使用して順番に表示させるプログラムを作成してください。
  */
 public class No010 {
 	public static void main(String[] args) {
