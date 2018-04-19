f8f7e5007a0ed1cfda023d2da72ea45b47c7fc8a
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-19 11:28:31 +0900

    2018.04.19

---
+++ practice/fundamentals/chapter4/sec1_method/No016.java
@@ -0,0 +1,7 @@
+package fundamentals.chapter4.sec1_method;
+/*
+ * 戻り値ありのメソッドを任意に作成し、メソッドを使用した結果を変数に入れずに出力するプログラムを作成してください。
+ */
+public class No016 {
+	
+}
---
+++ practice/fundamentals/chapter4/sec1_method/No018.java
@@ -0,0 +1,10 @@
+package fundamentals.chapter4.sec1_method;
+
+/*
+ * じゃんけんを行うクラスを作成してください。
+ * キーボード入力に対して、じゃんけんで「勝ち、負け、あいこ」を表示するプログラムを作成してください。
+ */
+public class No018 {
+	
+}
+
