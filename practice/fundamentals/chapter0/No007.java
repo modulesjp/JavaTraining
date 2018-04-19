eeef5c1f3517b69602be55fb63c16f95d3e56ec7
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-19 15:30:37 +0900

    2018.04.19

--- practice/fundamentals/chapter0/No007.java
+++ practice/fundamentals/chapter0/No007.java
@@ -1,7 +1,7 @@
 package fundamentals.chapter0;
 /*
  * Runnableインターフェースを実装した無名クラスを作成し、
- * その中で「Hello 無名クラス」を表示するプログラムを作成してください。。
+ * その中で「Hello 無名クラス」を表示するプログラムを作成してください。
  */
 public class No007 {
 	
