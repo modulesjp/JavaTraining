a980408852a1f0110b3ad89fbbbca58f68411c54
株式会社SHT <SHT@LAPTOP-B825J571>
2018-04-19 17:56:13 +0900

    2018.04.19

--- projects/fundamentals/chapter0/Lambda.java
+++ projects/fundamentals/chapter0/Lambda.java
@@ -1,5 +1,30 @@
 package fundamentals.chapter0;
 
-public class Lambda {
+import java.util.Random;
 
+public class Lambda {
+	public static void main(String[] args) {
+		int ぜろ = 0;
+		int いち = 0;
+		int に = 0;
+		Random r = new Random();
+		for(int i = 0; i < 1000; i++) {
+			int x = r.nextInt(3);
+			switch(x) {
+			case 0:
+				ぜろ += 1;
+				break;
+			case 1:
+				いち += 1;
+				break;
+			case 2:
+				に += 1;
+				break;
+			}
+		}
+		System.out.println("ぜろ = " + ぜろ);
+		System.out.println("いち = " + いち);
+		System.out.println("に = " + に);
+	}
+	
 }
